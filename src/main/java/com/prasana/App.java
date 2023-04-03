8d6c9917abab4b8b5181c848ca67bfb60fe97595
madhavidevopscloud <madhavipabba2023@gmail.com>
2023-04-03 10:33:47 +0530

    maven test

---
+++ src/main/java/com/prasana/App.java
@@ -0,0 +1,13 @@
+package com.prasana;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
